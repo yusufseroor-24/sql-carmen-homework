940604a7cf2df534f7236146c93266aa830fc81a
yusufseroor-24 <yusufseroor30@gmail.com>
2026-09-20 21:33:21 +0300

    clue #1

--- find_carmen.sql
+++ find_carmen.sql
@@ -1,6 +1,8 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+SELECT name FROM country 
+WHERE region = 'Southern Europe'
 
 
 
