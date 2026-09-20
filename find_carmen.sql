54e5936fbd33691bfae52751673f411efbef2a0c
yusufseroor-24 <yusufseroor30@gmail.com>
2026-09-20 22:15:18 +0300

    case #5

--- find_carmen.sql
+++ find_carmen.sql
@@ -41,6 +41,9 @@ WHERE co.name = 'San Marino' AND ci.name !='San Marino'
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+SELECT ci.name, co.name FROM city ci
+JOIN country co ON ci.countrycode = co.code
+WHERE ci.name LIKE 'Serra%' AND ci.name != 'Serravalle';
 
 
 
